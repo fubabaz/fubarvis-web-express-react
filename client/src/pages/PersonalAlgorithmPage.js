3e648c9e8c9b78fb2ea6dbc8351acbabdc021947
zeroradish <mr.zeroradish@gmail.com>
2024-06-26 22:54:12 +0900

    feat: 개인알고리즘 맴버 카드 프로필 사진영역에 티어 이미지 추가

--- client/src/pages/PersonalAlgorithmPage.js
+++ client/src/pages/PersonalAlgorithmPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import AlgorithmService from "../services/algorithmService";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import "react-perfect-scrollbar/dist/css/styles.css"; // PerfectScrollbar CSS 파일 로드
+import baekjoonLogo from "../assets/img/logo/baekjoon-logo.png";
+import solvedLogo from "../assets/img/logo/solved-logo.png";
 
 function HomePage() {
   const [individual, setIndividual] = useState([]);
@@ -32,15 +34,50 @@ function HomePage() {
         <div className="col-3" style={{ height: "calc(100vh - 150px)" }}>
           <PerfectScrollbar>
             {individual.map((data, index) => (
-              <div class="card mb-2 mx-4">
-                <div class="card-body">
+              <div class="card mb-3 me-3 bg-white rounded border-0 card">
+                <div
+                  class="card-body shadow-sm"
+                  style={{
+                    border: "1px solid #f7f7f7",
+                    borderRadius: "2px",
+                    fontSize: "13.5px",
+                  }}
+                >
+                  <div
+                    style={{ position: "relative", display: "inline-block" }}
+                  >
+                    <img
+                      height="28"
+                      width="28"
+                      src={data.image}
+                      className="rounded user-avatar"
+                      alt="User Avatar"
+                    />
+                    <img
+                      height="15"
+                      src={`${process.env.PUBLIC_URL}/assets/img/icon/${data.tier}.png`}
+                      className="rounded"
+                      alt="Trophy Icon"
+                      style={{
+                        position: "absolute",
+                        bottom: "3px",
+                        right: "2px",
+                        transform: "translate(50%, 50%)",
+                      }}
+                    />
+                  </div>
+                  <p className="font-weight-bold d-flex">{data.github_id}</p>
                   <img
-                    height="28"
-                    width="28"
-                    src={data.image}
-                    className="mx-2 mb-1 rounded user-avatar"
-                    alt="User Avatar"
+                    className="me-1"
+                    src={baekjoonLogo}
+                    style={{
+                      width: "20px",
+                      borderRadius: "3px",
+                    }}
                   />
+                  <small className="text-muted">
+                    해결한 문제 {data.sol_prob_cnt}건
+                  </small>
                 </div>
               </div>
             ))}
--- server/routes/algorithm.js
+++ server/routes/algorithm.js
@@ -73,6 +73,15 @@ router.get('/individual', async (req, res) => {
       SELECT * FROM (
         SELECT BAEKJOON_ID,
                    SOL_PROB_CNT,
+                   RANKING,
+                   SUBSTRING(SPLIT_PART(LEV, ' ', 1),1,1)||CASE 
+                   WHEN  SPLIT_PART(LEV, ' ', 2) = 'I' THEN '1'
+                   WHEN  SPLIT_PART(LEV, ' ', 2) = 'II' THEN '2'
+                   WHEN  SPLIT_PART(LEV, ' ', 2) = 'III' THEN '3'
+                   WHEN  SPLIT_PART(LEV, ' ', 2) = 'IV' THEN '4'
+                   WHEN  SPLIT_PART(LEV, ' ', 2) = 'V' THEN '5'
+                   ELSE 'UNRATED'
+               END AS TIER,
                  ROW_NUMBER() OVER(PARTITION BY BAEKJOON_ID ORDER BY BAEKJOON_ID, BASE_DT DESC) AS RN
           FROM RANKING_INFORMATION
           WHERE ACCOUNT_TYPE = 'I'
