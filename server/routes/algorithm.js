090e41529a79f41c469c5fc02f78f87b5f6619c6
zeroradish <mr.zeroradish@gmail.com>
2024-06-27 08:31:07 +0900

    fix: tier가 unrated인 사용자의  조회 오류  수정

--- server/routes/algorithm.js
+++ server/routes/algorithm.js
@@ -74,14 +74,13 @@ router.get('/individual', async (req, res) => {
         SELECT BAEKJOON_ID,
                    SOL_PROB_CNT,
                    RANKING,
-                   SUBSTRING(SPLIT_PART(LEV, ' ', 1),1,1)||CASE 
+                   CASE WHEN UPPER(LEV) !='UNRATED' THEN (SUBSTRING(SPLIT_PART(LEV, ' ', 1),1,1)||CASE 
                    WHEN  SPLIT_PART(LEV, ' ', 2) = 'I' THEN '1'
                    WHEN  SPLIT_PART(LEV, ' ', 2) = 'II' THEN '2'
                    WHEN  SPLIT_PART(LEV, ' ', 2) = 'III' THEN '3'
                    WHEN  SPLIT_PART(LEV, ' ', 2) = 'IV' THEN '4'
-                   WHEN  SPLIT_PART(LEV, ' ', 2) = 'V' THEN '5'
-                   ELSE 'UNRATED'
-               END AS TIER,
+                   WHEN  SPLIT_PART(LEV, ' ', 2) = 'V' THEN '5'               
+               END) ELSE UPPER(LEV)  END AS TIER,
                  ROW_NUMBER() OVER(PARTITION BY BAEKJOON_ID ORDER BY BAEKJOON_ID, BASE_DT DESC) AS RN
           FROM RANKING_INFORMATION
           WHERE ACCOUNT_TYPE = 'I'
