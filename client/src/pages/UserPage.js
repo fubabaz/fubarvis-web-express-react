352ed718447677ef356075880d1c4a99d503b89c
zeroradish <mr.zeroradish@gmail.com>
2024-06-30 08:35:46 +0900

    refactor: 미사용 코드 제거

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import UserProfileCard from '../components/UserProfileCard';
 import UserService from '../services/userService';
-import 'react-perfect-scrollbar/dist/css/styles.css';
 import PerfectScrollbar from 'react-perfect-scrollbar'
-function UserPage() {
-  const [users, setUsers] = useState([]);
+import users from '../data/users.json';
 
-  useEffect(() => {
-    // 페이지가 로드될 때 한 번만 유저 정보를 가져옴
+function UserPage() {
+  const handleCardClick = () => {
     UserService.getUsers().then((data) => {
-      console.log(data)
-      setUsers(data);
     });
-  }, []);
+  };
 
   return (
     <div>
       <div className="m-4">
       <h3>맴버 프로필</h3>
+      <button onClick={() => handleCardClick()}>click</button>
       </div>
       <PerfectScrollbar options={{ suppressScrollX: true }}>
       <div className="user-list row content">
