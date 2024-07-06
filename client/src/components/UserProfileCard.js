fec872fbcca7861bde655ded94f5919b428d8b33
zeroradish <mr.zeroradish@gmail.com>
2024-07-06 22:14:44 +0900

    feat: 맴버프로필 정보에 top language 영역 추가

--- client/src/components/UserProfileCard.js
+++ client/src/components/UserProfileCard.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import GitHubCalendar from "react-github-calendar";
 import { Modal, Button } from "react-bootstrap";
-import TrophyModal from "./modal/TrophyModal"; 
+import TrophyModal from "./modal/TrophyModal";
 
 import baekjoonLogo from "../assets/img/logo/baekjoon-logo.png";
 import solvedLogo from "../assets/img/logo/solved-logo.png";
@@ -12,49 +12,48 @@ const UserProfileCard = ({ user, col }) => {
   const [showModal, setShowModal] = useState(false);
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
-  
-  const cardClasses = ` bg-white rounded border-0 card p-2 my-3  ${
-    col ? `col-${col}` : ""
-  }`;
+
+  const cardClasses = ` bg-white rounded border-0 card p-2 my-3  ${col ? `col-${col}` : ""
+    }`;
   return (
     <>
       <div className={cardClasses}>
         <div className="card-body shadow-sm">
-        <div className="d-flex justify-content-start">
-          <div>
-            <img
-              size="32"
-              height="32"
-              width="32"
-              src={user.image}
-              className="mx-2 mb-1 rounded-circle user-avatar"
-              alt="User Avatar"
-            />
-          </div>
-
-          <div className="d-flex align-items-center">
-            <span style={{ fontWeight: "410" }}>{user.github_id}</span>
-          </div>
-
-          {user.trophy_cnt && (
-            <div
-              className="d-flex align-items-center mx-3"
-              onClick={handleShowModal}
-              style={{ cursor: "pointer" }}
-            >
+          <div className="d-flex justify-content-start">
+            <div>
               <img
-                height="25"
-                width="25"
-                src={throphyIcon}
-                className="ml-3 rounded-circle user-avatar"
-                alt="Trophy Icon"
+                size="32"
+                height="32"
+                width="32"
+                src={user.image}
+                className="mx-2 mb-1 rounded-circle user-avatar"
+                alt="User Avatar"
               />
-              <span className="ml-0 text-primary" style={{ fontWeight: "410" }}>
-                {user.trophy_cnt}
-              </span>
             </div>
-          )}
-        </div>
+
+            <div className="d-flex align-items-center">
+              <span style={{ fontWeight: "410" }}>{user.github_id}</span>
+            </div>
+
+            {user.trophy_cnt && (
+              <div
+                className="d-flex align-items-center mx-3"
+                onClick={handleShowModal}
+                style={{ cursor: "pointer" }}
+              >
+                <img
+                  height="25"
+                  width="25"
+                  src={throphyIcon}
+                  className="ml-3 rounded-circle user-avatar"
+                  alt="Trophy Icon"
+                />
+                <span className="ml-0 text-primary" style={{ fontWeight: "410" }}>
+                  {user.trophy_cnt}
+                </span>
+              </div>
+            )}
+          </div>
 
           <GitHubCalendar
             hideMonthLabels={true}
@@ -66,13 +65,20 @@ const UserProfileCard = ({ user, col }) => {
             fontSize={10}
             username={user.github_id}
           />
-           <div className="image-container">
-      <img 
-        src={`https://github-readme-stats.vercel.app/api?username=${user.github_id}&count_private=true&theme=graywhite&show_icons=true`}
-        alt="GitHub Stats" 
-        className="cropped-image"
-      />
-    </div>
+          <div className="image-container">
+            <img
+              src={`https://github-readme-stats.vercel.app/api?username=${user.github_id}&count_private=true&theme=graywhite&show_icons=true`}
+              alt="GitHub Stats"
+              className="cropped-image"
+            />
+
+            <img
+              src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${user.github_id}&count_private=true&layout=compact&theme=graywhite&hide_title=true`}
+              alt="Top Languages"
+              className="cropped-image"
+            />
+          </div>
+          <hr style={{ color: "#c4c4c4" }}></hr>
           <p className="card-text mb-1" style={{ fontSize: "13px" }}>
             <small className="text-muted">{user.joinedDate}</small>
           </p>
@@ -122,7 +128,7 @@ const UserProfileCard = ({ user, col }) => {
         trophyCount={user.trophy_cnt}
         baekjoonId={user.baekjoon_id}
         image={user.image}
-        
+
       />
     </>
   );
